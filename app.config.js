77ed306778d2a5302f4c128b4649ad8cafa04e7e
Leonardo Bezerra <leonardoalvesbezerra@gmail.com>
2025-07-11 17:10:53 -0300

    fix: change localhost port to match recipeservice (8080 -> 8001)

--- app.config.js
+++ app.config.js
@@ -45,8 +45,8 @@ export default {
             typedRoutes: true
         },
         extra: {
-          API_BASE_URL: process.env.API_BASE_URL || "http://localhost:8080",
-          API_USER_URL: process.env.API_USER_URL || "http://localhost:8080"
+          API_BASE_URL: process.env.API_BASE_URL || "http://localhost:8001",
+          API_USER_URL: process.env.API_USER_URL || "http://localhost:8001"
         },
     }
 } 
